0fd667889ff1b834399ccf77311391e4c7b932cd
TilmanK <tilman.krummeck@googlemail.com>
2020-02-13 11:02:49 +0100

    Refactored TreeViewModel: - PEP8 and PEP257. - Type annotations. - Refactored add_item method and fixed Nodes under root (different handling needed due to asyncua). - More Qt-like approach in some methods to use QModelIndex.isValid() instead of checking UserData. - Fixed hasChildren method to avoid expand icon on item in view.

--- uawidgets/tree_widget.py
+++ uawidgets/tree_widget.py
@@ -1,10 +1,13 @@
-from typing import Optional
+import logging
+from typing import Optional, List, Iterable
 
-from PyQt5.QtCore import pyqtSignal, QMimeData, QObject, Qt, QSettings
+from PyQt5.QtCore import pyqtSignal, QMimeData, QObject, Qt, QSettings, \
+    QModelIndex
 from PyQt5.QtGui import QStandardItemModel, QStandardItem, QIcon
 from PyQt5.QtWidgets import QApplication, QAbstractItemView, QAction
 
-from asyncua.sync import ua
+from asyncua.ua import ReferenceDescription, ObjectIds, TwoByteNodeId, \
+    NodeClass
 from asyncua.sync import Node
 from asyncua.ua import UaError
 
@@ -12,17 +15,17 @@ from asyncua.ua import UaError
 class TreeWidget(QObject):
 
     error = pyqtSignal(Exception)
+    HEADER_LABELS = ['DisplayName', "BrowseName", 'NodeId']
 
     def __init__(self, view):
         QObject.__init__(self, view)
         self.view = view
         self.model = TreeViewModel()
-        self.model.clear()  # FIXME: do we need this?
-        self.model.error.connect(self.error)
+        self.model.clear()
         self.view.setModel(self.model)
 
         #self.view.setUniformRowHeights(True)
-        self.model.setHorizontalHeaderLabels(['DisplayName', "BrowseName", 'NodeId'])
+        self.model.setHorizontalHeaderLabels(TreeWidget.HEADER_LABELS)
         self.view.header().setSectionResizeMode(0)
         self.view.header().setStretchLastSection(True)
         self.view.setSelectionBehavior(QAbstractItemView.SelectRows)
@@ -121,6 +124,7 @@ class TreeWidget(QObject):
     def reload(self, item=None):
         if item is None:
             item = self.model.item(0, 0)
+            node = item.data(Qt.UserRole)
         for _ in range(item.rowCount()):
             child_it = item.child(0, 0)
             node = child_it.data(Qt.UserRole)
@@ -134,143 +138,145 @@ class TreeWidget(QObject):
             #if self.view.isExpanded(idx):
             #self.view.setExpanded(idx, True)
 
-    def remove_current_item(self):
+    def remove_current_item(self) -> None:
+        """Removes the current item from the model."""
         idx = self.view.currentIndex()
         self.model.removeRow(idx.row(), idx.parent())
 
     def get_current_node(self) -> Optional[Node]:
         idx = self.view.currentIndex()
-        idx = idx.sibling(idx.row(), 0)
-        it = self.model.itemFromIndex(idx)
-        if not it:
-            return None
-        node = it.data(Qt.UserRole)
-        if not node:
-            ex = RuntimeError("Item does not contain node data, report!")
-            self.error.emit(ex)
-            raise ex
+        msg = 'Current Index: row: %s, col: %s, parent: %s, parent_valid: %s'
+        logging.debug(msg, idx.row(), idx.column(), idx.parent(),
+                      idx.isValid())
+        node = self.model.itemFromIndex(idx).data(Qt.UserRole)
+        logging.debug("Got node for Index: %s", node)
         return node
 
 
 class TreeViewModel(QStandardItemModel):
+    """Tree view model containing Nodes of the connected server."""
 
-    error = pyqtSignal(Exception)
-
-    def __init__(self):
+    # pylint: disable=invalid-name
+    def __init__(self) -> None:
+        """Create a new TreeViewModel."""
         super(TreeViewModel, self).__init__()
-        self._fetched = []
+        self._fetched: List[Node] = []
 
-    def clear(self):
-        # remove all rows but not header!!
+    def clear(self) -> None:
+        """Remove all items and reset the header."""
         self.removeRows(0, self.rowCount())
         self._fetched = []
 
-    def set_root_node(self, node):
-        desc = self._get_node_desc(node)
-        self.add_item(desc, node=node)
+    def set_root_node(self, node: Node) -> None:
+        """Set the root node for the model."""
+        assert not self._fetched
+        description = self._get_node_desc(node)
+        self.add_item_by_node(description, node=node)
 
-    def _get_node_desc(self, node):
-        attrs = node.get_attributes([ua.AttributeIds.DisplayName, ua.AttributeIds.BrowseName, ua.AttributeIds.NodeId, ua.AttributeIds.NodeClass])
-        desc = ua.ReferenceDescription()
-        desc.DisplayName = attrs[0].Value.Value
-        desc.BrowseName = attrs[1].Value.Value
-        desc.NodeId = attrs[2].Value.Value
-        desc.NodeClass = attrs[3].Value.Value
-        desc.TypeDefinition = ua.TwoByteNodeId(ua.ObjectIds.FolderType)
-        return desc
+    @staticmethod
+    def _get_node_desc(node: Node) -> ReferenceDescription:
+        """Get the ReferenceDescription of a node."""
+        description = ReferenceDescription()
+        description.DisplayName = node.get_display_name()
+        description.BrowseName = node.get_browse_name()
+        description.NodeId = node.nodeid
+        description.NodeClass = node.get_node_class()
+        description.TypeDefinition = TwoByteNodeId(ObjectIds.FolderType)
+        return description
 
-    def add_item(self, desc, parent=None, node=None):
-        dname = bname = nodeid = "No Value"
-        if desc.DisplayName:
-            dname = desc.DisplayName.to_string()
-        if desc.BrowseName:
-            bname = desc.BrowseName.to_string()
+    def add_item_with_parent(self, desc: ReferenceDescription,
+                             parent: QStandardItem) -> None:
+        """Add an item to the model with the given parent."""
+        parent_node = parent.data(Qt.UserRole)
+        node = parent_node.get_child(desc.BrowseName.to_string())
+        item = self._create_items(desc, node)
+        parent.appendRow(item)
+
+    def add_item_by_node(self, desc: ReferenceDescription, node: Node) -> None:
+        """Add an item to the model."""
+        item = self._create_items(desc, node)
+        self.appendRow(item)
+
+    def _create_items(self, desc: ReferenceDescription, node: Node)\
+            -> List[QStandardItem]:
+        """Create a list of items from a description and a node."""
+        dname = desc.DisplayName.to_string()
+        bname = desc.BrowseName.to_string()
         nodeid = desc.NodeId.to_string()
-        item = [QStandardItem(dname), QStandardItem(bname), QStandardItem(nodeid)]
-        if desc.NodeClass == ua.NodeClass.Object:
-            if desc.TypeDefinition == ua.TwoByteNodeId(ua.ObjectIds.FolderType):
-                item[0].setIcon(QIcon(":/folder.svg"))
-            else:
-                item[0].setIcon(QIcon(":/object.svg"))
-        elif desc.NodeClass == ua.NodeClass.Variable:
-            if desc.TypeDefinition == ua.TwoByteNodeId(ua.ObjectIds.PropertyType):
-                item[0].setIcon(QIcon(":/property.svg"))
-            else:
-                item[0].setIcon(QIcon(":/variable.svg"))
-        elif desc.NodeClass == ua.NodeClass.Method:
-            item[0].setIcon(QIcon(":/method.svg"))
-        elif desc.NodeClass == ua.NodeClass.ObjectType:
-            item[0].setIcon(QIcon(":/object_type.svg"))
-        elif desc.NodeClass == ua.NodeClass.VariableType:
-            item[0].setIcon(QIcon(":/variable_type.svg"))
-        elif desc.NodeClass == ua.NodeClass.DataType:
-            item[0].setIcon(QIcon(":/data_type.svg"))
-        elif desc.NodeClass == ua.NodeClass.ReferenceType:
-            item[0].setIcon(QIcon(":/reference_type.svg"))
-        if node:
-            assert isinstance(node, Node)
-            item[0].setData(node, Qt.UserRole)
-        else:
-            parent_node = parent.data(Qt.UserRole)
-            item[0].setData(Node(parent_node.aio_obj.server, desc.NodeId), Qt.UserRole)
-        if parent:
-            return parent.appendRow(item)
-        else:
-            return self.appendRow(item)
+        items = [QStandardItem(dname),
+                 QStandardItem(bname),
+                 QStandardItem(nodeid)]
+        self._add_icon_to_item(desc, items[0])
+        items[0].setData(node, Qt.UserRole)
+        return items
 
-    def reset_cache(self, node):
-        if node in self._fetched:
+    @staticmethod
+    def _add_icon_to_item(desc: ReferenceDescription, item: QStandardItem)\
+            -> None:
+        """Add an icon to the item based on the description."""
+        if desc.NodeClass == NodeClass.Object:
+            if desc.TypeDefinition == TwoByteNodeId(ObjectIds.FolderType):
+                item.setIcon(QIcon("uawidgets/folder.svg"))
+            else:
+                item.setIcon(QIcon("uawidgets/object.svg"))
+        elif desc.NodeClass == NodeClass.Variable:
+            if desc.TypeDefinition == TwoByteNodeId(ObjectIds.PropertyType):
+                item.setIcon(QIcon("uawidgets/property.svg"))
+            else:
+                item.setIcon(QIcon("uawidgets/variable.svg"))
+        elif desc.NodeClass == NodeClass.Method:
+            item.setIcon(QIcon("uawidgets/method.svg"))
+        elif desc.NodeClass == NodeClass.ObjectType:
+            item.setIcon(QIcon("uawidgets/object_type.svg"))
+        elif desc.NodeClass == NodeClass.VariableType:
+            item.setIcon(QIcon("uawidgets/variable_type.svg"))
+        elif desc.NodeClass == NodeClass.DataType:
+            item.setIcon(QIcon("uawidgets/data_type.svg"))
+        elif desc.NodeClass == NodeClass.ReferenceType:
+            item.setIcon(QIcon("uawidgets/reference_type.svg"))
+        else:
+            logging.warning("Could not set item for desc %s", desc)
+
+    def reset_cache(self, node: Node) -> None:
+        """Reset the internal cache for the given node."""
+        try:
             self._fetched.remove(node)
+        except ValueError:
+            pass
 
-    def canFetchMore(self, idx):
-        item = self.itemFromIndex(idx)
-        if not item:
+    def canFetchMore(self, parent: QModelIndex) -> bool:
+        """Return if more items can be fetched for the given parent."""
+        if not parent.isValid():
             return False
-        node = item.data(Qt.UserRole)
+        node = self.itemFromIndex(parent).data(Qt.UserRole)
         if node not in self._fetched:
             self._fetched.append(node)
             return True
         return False
 
-    def hasChildren(self, idx):
-        item = self.itemFromIndex(idx)
-        if not item:
+    def hasChildren(self, idx: QModelIndex = QModelIndex()) -> bool:
+        """Return if the given index has children."""
+        if not idx.isValid():
+            # if the index isn't valid, it's the root of the TreeView
             return True
-        node = item.data(Qt.UserRole)
-        if node in self._fetched:
-            return QStandardItemModel.hasChildren(self, idx)
-        return True
+        node = self.itemFromIndex(idx).data(Qt.UserRole)
+        # Todo: Refactor to not cause a request every time method is called
+        return bool(node.get_children_descriptions())
 
-    def fetchMore(self, idx):
+    def fetchMore(self, idx: QModelIndex) -> None:
+        """Fetch and publish the children for the given index."""
         parent = self.itemFromIndex(idx)
-        if parent:
-            self._fetchMore(parent)
+        node = parent.data(Qt.UserRole)
+        descriptions = node.get_children_descriptions()
+        descriptions.sort(key=lambda x: x.BrowseName)
+        for desc in descriptions:
+            self.add_item_with_parent(desc, parent)
 
-    def _fetchMore(self, parent):
-        try:
-            node = parent.data(Qt.UserRole)
-            assert isinstance(node, Node)
-            descs = node.get_children_descriptions()
-            descs.sort(key=lambda x: x.BrowseName)
-            added = []
-            for desc in descs:
-                if not desc.NodeId in added:
-                    self.add_item(desc, parent)
-                    added.append(desc.NodeId)
-        except Exception as ex:
-            self.error.emit(ex)
-            raise
-
-    def mimeData(self, idxs):
+    def mimeData(self, indexes: Iterable[QModelIndex]) -> QMimeData:
+        """Return a QMimeData object for the given indexes."""
+        items = [self.itemFromIndex(idx) for idx in indexes]
+        nodes = [item.data(Qt.UserRole) for item in items]
+        node_ids = [node.nodeid.to_string() for node in nodes if node]
         mdata = QMimeData()
-        nodes = []
-        for idx in idxs:
-            item = self.itemFromIndex(idx)
-            if item:
-                node = item.data(Qt.UserRole)
-                if node:
-                    nodes.append(node.nodeid.to_string())
-        mdata.setText(", ".join(nodes))
+        mdata.setText(", ".join(node_ids))
         return mdata
-
-
